debf8eea37d470b9d98cfcb78aeb175ca451609a
Malak <szmajdakarol@gmail.com>
2021-10-24 20:53:12 +0200

    Dodaj pliki projektów.

---
+++ s03-countdown/s03-countdown.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using std::logic_error;
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::stoi;
+using std::endl;
+
+auto main(int argc, const char* argv[]) -> int
+{
+	try
+	{
+		if (argc != 2)
+		{
+			throw logic_error{ "Podana ilosc argumentow jest nieprawidlowa." };
+		}
+
+		int N = stoi(argv[1]);
+
+		for (int i = N; i != 0; )
+		{
+			cout << i << "..." << endl;
+
+			if (N > 0)
+			{
+				i--;
+			}
+			else 
+			{
+				i++;
+			}
+		}
+
+		cout << "0..." << endl;
+
+		return 0;
+	}
+	catch (logic_error ex)
+	{
+		cerr << ex.what() << endl;
+		return 1;
+	}
+}
